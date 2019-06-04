8957fcca8b4e0d119a8dfef4d8a219b55b60a791
fluzzyy <gunte95@hotmail.com>
2019-06-04 14:16:07 +0200

    wtf

---
+++ Frontend/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <counter-button />
+    <cards />
+  </div>
+</template>
+<script>
+  import CounterButton from './CounterButton.vue'
+  import Cards from './Cards.vue'
+
+  export default {
+    components: {
+       'counter-button': CounterButton,
+       'cards': Cards
+    }
+  }
+</script>
