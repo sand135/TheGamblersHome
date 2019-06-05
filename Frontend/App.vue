f402b5213a37ed9b43ec2027865c47c1cf7e778b
jobe95 <jonatanbengtsson95@hotmail.se>
2019-06-05 15:05:23 +0200

    Japp

--- Frontend/App.vue
+++ Frontend/App.vue
@@ -5,13 +5,11 @@
   </div>
 </template>
 <script>
-  import CounterButton from './CounterButton.vue'
   import Card from './Card.vue'
   import PokerTable from './PokerTable.vue'
 
   export default {
     components: {
-       'counter-button': CounterButton,
        'card': Card,
        'poker-table': PokerTable
     }
