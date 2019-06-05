ec3581bef6950dccf4f8ddab48569de5b6208ecb
sand135 <sasundqv@gmail.com>
2019-06-05 09:41:44 +0200

    edited files after merge

--- Frontend/loginFunctionality/SignInUp.vue
+++ Frontend/loginFunctionality/SignInUp.vue
@@ -1,3 +1,44 @@
-<template id="signInUp">
-  <h1>hej</h1>
+<template>
+  <div id="signInUpButtons">
+    <button class="button" v-on:click="redirectToLogin()">Log in</button>
+
+    <button class="button" v-on:click="redirectToRegister()">Register</button>
+  </div>
+
 </template>
+
+<script type="text/javascript">
+
+
+  export default {
+    name: 'SignInUp',
+
+    methods: {
+      redirectToLogin(){
+        this.$router.replace({ name: "SignIn" })
+      },
+
+      redirectToRegister(){
+        this.$router.replace({ name: "Register" })
+      }
+    }
+  }
+
+
+
+</script>
+
+<style>
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 25px;
+
+}
+</style>
