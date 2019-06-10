8daea6ed7c47e2c9c6ae3f27ea187b4dc83c50db
fluzzyy <gunte95@hotmail.com>
2019-06-10 09:53:44 +0200

    github funkar ju inte

--- Frontend/PokerTable.vue
+++ Frontend/PokerTable.vue
@@ -1,31 +1,51 @@
 <template>
- <div>
- <img src="poker-table-drawing-11.png" id="backgroundImage">
-    <!-- <img src="Images/Playing_cards/2_of_clubs.png" id="tablecard1"> -->
-            <!-- <img src="Images/Playing_cards/2_of_diamonds.png" id="tablecard2">
-            <img src="Images/Playing_cards/2_of_hearts.png" id="tablecard3">
-            <img src="Images/Playing_cards/2_of_spades.png" id="tablecard4">
-             <img src="Images/Playing_cards/3_of_clubs.png" id="tablecard5"> -->
-            <picture id="avatar1" class="player1">
-                <img src ="Images/player_avatars/avatar1.png" class="avatar">
-                <div id="player1" class="player1">Kalle</div>
-            </picture>
-             <img src ="Images/player_avatars/avatar2.png" id="avatar2" class="avatar"> -->
- </div>
+  <div>
+     
+    <div id="bg">
+      <img src="poker-table-drawing-11.png">
+    </div>
+        
+      
+      <div id="player1">
+      <img src="Images/player_avatars/avatar1.png" class="avatar">
+      <div class="playerNameText">JONATAN</div>
+    <div>
+    <img src="Images/Playing_cards/backsides/playingcard_backside.jpg" id="player1_card1" class="playercards">
+    <img src="Images/Playing_cards/backsides/playingcard_backside.jpg" id="player1_card2" class="playercards">
+    </div>
+    </div>
+
+      <div id="player2">
+      <img src="Images/player_avatars/avatar2.png" class="avatar">
+      <div class="playerNameText">ALEXANDER</div>
+    <img src="Images/Playing_cards/backsides/playingcard_backside.jpg" id="player2_card1" class="playercards">
+    <img src="Images/Playing_cards/backsides/playingcard_backside.jpg" id="player2_card2" class="playercards">
+    </div>
+    
+    <img src="Images/Playing_cards/2_of_clubs.png" id="tablecard1" class="tablecards">
+    <img src="Images/Playing_cards/2_of_diamonds.png" id="tablecard2" class="tablecards">
+    <img src="Images/Playing_cards/2_of_hearts.png" id="tablecard3" class="tablecards">
+    <img src="Images/Playing_cards/2_of_spades.png" id="tablecard4" class="tablecards">
+    <img src="Images/Playing_cards/3_of_clubs.png" id="tablecard5" class="tablecards">
+
+    
+    <div id="totalpot">
+        Total pot: {{ this.potMoney }}
+            </div>
+    
+  
+  </div>
 </template>
 <script>
-
-  export default {
-    data() {
-      return {
-        
-      }
-    },
-    methods: {
-
-    }
+export default {
+  data() {
+    
+    return {};
+  },
+  methods: {
+    onClick() {}
   }
-
+};
 </script>
 <style scoped>
 
@@ -48,7 +68,7 @@
   min-width: 45%;
   min-height: 45%;
 
-
+}
 
 /* #thetable {
   /* position: relative;
@@ -65,30 +85,75 @@
   overflow: auto;
 } */
 
-
-
- }
- #tablecard5 {
+#totalpot {
     position: fixed;
-    bottom: 300px;
-    right: 470px;
-    width: 100px;
+    bottom: 65%;
+    right: 50%;
+    color: black;
+    font-size: 200%;
+}
 
- }
- #avatar1 {
-        position: fixed;
-    bottom: 100px;
-    right: 180px;
-    width: 100px;
-    color: blue;
+#tablecard1 {
+  position: fixed;
+  bottom: 40%;
+  right: 61.5%;
 
- }
+}
 
-#avatar2 {
-            position: fixed;
-    bottom: 550px;
-    right: 150px;
-    width: 100px;
- }
+#tablecard2 {
+  position: fixed;
+  bottom: 40%;
+  right: 54%;
+}
+#tablecard3 {
+  position: fixed;
+  bottom: 40%;
+  right: 46.5%;
+}
+#tablecard4 {
+  position: fixed;
+  bottom: 40%;
+  right: 39%;
+} 
+#tablecard5 {
+  position: fixed;
+  bottom: 40%;
+  right: 31.5%;
+} 
 
+.tablecards {
+  z-index: 2;
+  width: 7%;
+}
+#player1 {
+  position: fixed;
+  bottom: 80%;
+  left: 85%; 
+  
+  
+}
+
+#player2 {
+  position: fixed;
+  bottom: 12%;
+  left: 85%;
+}
+
+.avatar {
+  width: 25%;
+}
+
+.playerNameText {
+  color: green;
+  text-align: left;
+  font-weight: bold;
+  font-size: 1em;
+  font-family: sans-serif;
+}
+
+
+.playercards {
+    
+    width: 15%; 
+}
 </style>
